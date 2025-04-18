b4b68deeff310d40d856e077aa443b6ba38fc1e9
whoevericanget <upgoes13@gmail.com>
2025-04-18 20:40:49 +0100

    Create um.js

---
+++ um.js
@@ -0,0 +1,28 @@
+function dt() {
+  const date = new Date();
+  date.setTime(date.getTime());
+  const dateString = date.getTime() + '|' + date.toISOString() + '|' + Math.random();
+  const key = new Uint8Array([0x68]);
+  const textEncoder = new TextEncoder();
+  const resultBuffer = textEncoder.encode(dateString);
+  const originalBuffer = new Uint8Array(resultBuffer.length);
+  for (let i = 0; i < resultBuffer.length; i++) {
+    originalBuffer[i] = resultBuffer[i] ^ key[i % key.length];
+  }
+  const convertedDate = Array.from(originalBuffer).map(byte => byte.toString(16).padStart(2, '0')).join('');
+  return convertedDate;
+}
+
+const t = JSON.parse(localStorage['user:token']).data;
+const id = "KOIQUBLRTTLT";
+
+fetch(
+  `https://free4talk-identity.herokuapp.com/identity/post/unfollow/?a=identity-post-follow&v=513-1&t=${Date.now()}`,
+  {
+  "method": "POST",
+  "body": `{
+    \"token\":\"${t}\",
+    \"body\":{\"toId\":\"${id}\"},
+    \"_\":\"${dt()}\"
+  }`,
+});
